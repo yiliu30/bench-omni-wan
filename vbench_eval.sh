f637316d25187e8755abe0efba76da64bf3f6d45
yiliu30 <yi4.liu@intel.com>
2026-05-19 03:34:12 +0000

    fix
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- vbench_eval.sh
+++ vbench_eval.sh
@@ -1,5 +1,6 @@
 vbench_dir=/home/yiliu7/workspace/VBench
 video_dir=/home/yiliu7/workspace/vllm-omni/bench-wan/output/default_bf16
+video_dir=/mnt/ctrl/disk2/yiliu7/wan-res/default_real_sage3_attn_only/
 # video_dir=/home/yiliu7/workspace/vllm-omni/bench-wan/output/default_mxfp4_linear_only
 test_basename=$(basename $video_dir)
 test_name="${test_basename}_subject_consistency"
